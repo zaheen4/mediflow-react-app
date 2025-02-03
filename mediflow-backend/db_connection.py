7a6f39dd06cc411c7ebf1592477d72ef9b3e2ef6
Zaheen Waseet <zwaseet@gmail.com>
2025-02-03 20:22:31 +0600

    Useless tests

--- mediflow-backend/db_connection.py
+++ mediflow-backend/db_connection.py
@@ -1,19 +1,42 @@
-# import mysql.connector
+import mysql.connector
+
+def get_db_connection():
+    # Replace these values with your MySQL credentials
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="zaheen",  # Replace with your MySQL username
+        password="0000",  # Replace with your MySQL password
+        database="MediFlowDB"  # Replace with your MySQL database name
+    )
+    return connection
+
+
+# import os
+# from sqlalchemy import create_engine
+
+# DATABASE_URL = os.getenv("DATABASE_URL", "mysql://root:mYAkEoNRZlrZBHJtXgQnuFNUKsvKuXaE@viaduct.proxy.rlwy.net:23266/railway")
+
+# engine = create_engine(DATABASE_URL)
+
+
+# import os
+# import sqlalchemy
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
+# # Fetch DATABASE_URL from Railway environment variables
+# DATABASE_URL = os.getenv("mysql://root:mYAkEoNRZlrZBHJtXgQnuFNUKsvKuXaE@viaduct.proxy.rlwy.net:23266/railway")
+
+# # Example connection string for SQL Server
+# # "mssql+pyodbc://username:password@host:port/database?driver=ODBC+Driver+17+for+SQL+Server"
+# engine = create_engine(DATABASE_URL, echo=True)
+
+# # Create a session
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # def get_db_connection():
-#     # Replace these values with your MySQL credentials
-#     connection = mysql.connector.connect(
-#         host="localhost",
-#         user="zaheen",  # Replace with your MySQL username
-#         password="0000",  # Replace with your MySQL password
-#         database="MediFlowDB"  # Replace with your MySQL database name
-#     )
-#     return connection
-
-
-import os
-from sqlalchemy import create_engine
-
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql://root:mYAkEoNRZlrZBHJtXgQnuFNUKsvKuXaE@viaduct.proxy.rlwy.net:23266/railway")
-
-engine = create_engine(DATABASE_URL)
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
